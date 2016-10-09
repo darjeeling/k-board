f99e26aea8ff0d9a4feff381d813317ce661804b
cjh5414 <cjh5414@gmail.com>
2016-10-09 15:04:56 +0900

    add functional test for pagination

--- kboard/board/models.py
+++ kboard/board/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-
 from django_summernote import models as summer_model
 from django_summernote import fields as summer_fields
 from django.core.paginator import Paginator
