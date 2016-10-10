f6d2ce0ef819ba0728139d51514f0e0f01e4a2e5
전현준 <guswnsxodlf@gmail.com>
2016-10-10 20:27:58 +0900

    Modify 'view_post' preparing to POST request
    Add 'get_absolute_url()' on model classes
    
    Add 'new_comment' view, Remove POST logic in 'view_post' view

--- kboard/board/models.py
+++ kboard/board/models.py
@@ -1,13 +1,21 @@
 from django.db import models
+
 from django_summernote import models as summer_model
 from django_summernote import fields as summer_fields
 from django.core.paginator import Paginator
+from django.core.urlresolvers import reverse
 
 class Board(models.Model):
+    def get_absolute_url(self):
+        return reverse('board:post_list', args=[self.id])
+
     name = models.TextField(default='')
 
 
 class Post(models.Model):
+    def get_absolute_url(self):
+        return reverse('board:view_post', args=[self.id])
+
     title = models.TextField(default='')
     content = models.TextField(default='')
     board = models.ForeignKey(Board, null=True)
--- kboard/board/views.py
+++ kboard/board/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-from board.models import Post, Board
+from board.models import Post, Board, Comment
 from board.forms import PostForm
 
 
@@ -34,9 +34,9 @@ def post_list(request, board_id):
 
 
 def view_post(request, post_id):
-    post_ = Post.objects.get(id=post_id)
+    post = Post.objects.get(id=post_id)
 
-    return render(request, 'view_post.html', {'post': post_, 'board_id': request.GET['board']})
+    return render(request, 'view_post.html', {'post': post, 'board_id': post.board.id})
 
 
 def board_list(request):
@@ -47,3 +47,10 @@ def board_list(request):
     boards = Board.objects.all()
 
     return render(request, 'board_list.html', {'boards': boards})
+
+
+def new_comment(request):
+    if request.method == 'POST':
+        post = Post.objects.get(id=request.POST['post_id'])
+        Comment.objects.create(post=post, content=request.POST['comment_content'])
+        return redirect(post)
