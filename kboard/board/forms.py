59925a3951ea6a95ab9e735a1e31197b67012999
hyesun03 <chsun0303@gmail.com>
2016-10-29 15:05:23 +0900

    Customize registration form

--- kboard/board/forms.py
+++ kboard/board/forms.py
@@ -1,7 +1,25 @@
 from django import forms
+from django.contrib.auth import get_user_model
+
+from registration.forms import RegistrationForm
 from django_summernote.widgets import SummernoteWidget, SummernoteInplaceWidget
 from django_summernote import fields as summer_fields
+
 from .models import SummerNote
+from .models import BoardUser
+
+
+attrs_dict = { 'class': 'required' }
+
+def affiliation_valid_email(value):
+    allowed_domain = ['naver.com','gmail.com']
+    #not the prettiest soliution
+    v_error = ("Email does not match allowed domains (%s, %s)"
+                           %(allowed_domain[0],allowed_domain[1]))
+    if value.split('@')[1] not in allowed_domain:
+        raise forms.ValidationError(v_error)
+    else:
+        return value
 
 
 class PostForm(forms.ModelForm):
@@ -10,3 +28,20 @@ class PostForm(forms.ModelForm):
      class Meta:
            model = SummerNote
            fields = ('fields', )
+
+
+class CustomizeRegistrationForm(forms.ModelForm):
+    password1 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput
+    )
+
+    password2 = forms.CharField(
+        label="Password confirmation",
+        widget=forms.PasswordInput,
+        help_text="Enter the same password as above, for verification."
+    )
+
+    class Meta:
+        model = get_user_model()
+        fields = (model.USERNAME_FIELD, ) + ('password1', 'password2', ) + tuple(model.REQUIRED_FIELDS)
--- kboard/board/models.py
+++ kboard/board/models.py
@@ -1,5 +1,7 @@
 from django.core.urlresolvers import reverse
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 from django_summernote import fields as summer_fields
 from django_summernote import models as summer_model
 
@@ -35,3 +37,12 @@ class Comment(TimeStampedModel):
     content = models.TextField(default='')
     post = models.ForeignKey(Post, null=True)
     is_deleted = models.BooleanField(default=False)
+
+
+class BoardUser(AbstractUser):
+    username = models.CharField(max_length=30, unique=True)
+    full_name = models.CharField(max_length=50, default='')
+    email = models.EmailField(default='')
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['full_name', 'email', ]
