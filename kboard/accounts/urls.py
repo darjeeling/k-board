bfdc4f6d7c8320716108f7fc6aa270d6193766cc
hyesun03 <chsun0303@gmail.com>
2016-11-12 04:34:49 +0900

    Reset url to make password reset feature

--- kboard/accounts/urls.py
+++ kboard/accounts/urls.py
@@ -1,15 +1,8 @@
-from django.conf.urls import url, include
-from accounts.views import RegistrationView
-from accounts.forms import RegistrationForm
+from django.conf.urls import url
 
 from . import views
 
 app_name = 'accounts'
 urlpatterns = [
-    url(r'^register/$',
-        RegistrationView.as_view(
-            form_class=RegistrationForm
-        ), name='registration_register'),
     url(r'^profile/$', views.profile, name='profile'),
-    url(r'^', include('registration.backends.hmac.urls')),
 ]
